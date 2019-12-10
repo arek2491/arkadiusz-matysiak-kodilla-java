d8f3f63ac272e15caca9b5d47c516acb7bf35a78
arek2491 <arkadiusz.matysiak@gmail.com>
2019-12-10 18:10:07 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,41 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result=simpleUser.getUsername();
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }
+        else {
+            System.out.println("Error");
+        }
+
+        System.out.println("Calculator Test: ");
+        System.out.println("Add test: ");
+        Calculator calculator = new Calculator();
+        int result1 = calculator.add(10,15);
+        if(result1 == 25) {
+            System.out.println("test OK");
+        }
+        else{
+            System.out.println("Error");
+        }
+
+        System.out.println("Subtract test: ");
+        int result2 = calculator.subtract(20,18);
+        if(result2 == 2){
+            System.out.println("test OK");
+        }
+        else{
+            System.out.println("Error");
+        }
+
+
     }
 }
