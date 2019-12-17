0865f4eb6076ed24bf18b3cb150608b3f65e3672
arek2491 <arkadiusz.matysiak@gmail.com>
2019-12-17 18:14:26 +0100

    First Commit

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("This is a beautiful text" , text -> "ABC " + text + " ABC");
+        poemBeautifier.beautify("I don't know if it's a good solution" , String::toUpperCase);
+        poemBeautifier.beautify("My beautifier " , text -> "*** " + text + "***");
+        poemBeautifier.beautify("This is the last text" , text -> "~~~" + text + "~~~");
+    }
+
+
+}
