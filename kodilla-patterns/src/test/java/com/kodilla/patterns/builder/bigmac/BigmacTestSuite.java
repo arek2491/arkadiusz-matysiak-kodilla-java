123af333b42e20e8d42756e8739bdf6afbeb747c
arek2491 <arkadiusz.matysiak@gmail.com>
2020-01-23 15:20:08 +0100

    Zadanie: Komponujemy wlasnego Bigmaca

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -3,13 +3,16 @@ package com.kodilla.patterns.builder.bigmac;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BigmacTestSuite {
 
     @Test
     public void testNewBigmac() {
 
         Bigmac bigmac = new Bigmac.BigmacBuilder()
-                .bun("Bun without sezam")
+                .bun("bun without sezam")
                 .burgers(2)
                 .ingredient("cheese")
                 .sauce("barbecue")
@@ -19,6 +22,16 @@ public class BigmacTestSuite {
                 .build();
 
         int numOfIngredients = bigmac.getIngredients().size();
+        List<String> ingradientsTestList = new ArrayList<>();
+        ingradientsTestList.add("cheese");
+        ingradientsTestList.add("chili");
+        ingradientsTestList.add("onion");
+        ingradientsTestList.add("cucumber");
+
         Assert.assertEquals(4, numOfIngredients);
+        Assert.assertEquals("bun without sezam", bigmac.getBun());
+        Assert.assertEquals(2, bigmac.getBurgers());
+        Assert.assertEquals("barbecue", bigmac.getSauce());
+        Assert.assertEquals(ingradientsTestList, bigmac.getIngredients());
     }
 }
